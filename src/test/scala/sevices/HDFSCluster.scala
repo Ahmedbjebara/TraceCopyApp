1e36080b9761ba3b08cd5531e5dca7b95bf73648
nidhal.saadaoui1@esprit.tn <Azerty.199346>
2019-11-15 13:51:16 +0100

    hdfs mini cluster

--- src/test/scala/sevices/HDFSCluster.scala
+++ src/test/scala/sevices/HDFSCluster.scala
@@ -4,16 +4,18 @@ import java.io.File
 import java.util
 
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.hdfs.MiniDFSCluster
+import org.apache.hadoop.fs.LocalFileSystem
+import org.apache.hadoop.hdfs.{DistributedFileSystem, MiniDFSCluster}
 import org.apache.hadoop.test.PathUtils
 
 
 trait HDFSCluster {
   @transient private var hdfsCluster: MiniDFSCluster = _
   def startHDFS: Unit = {
-
     println("Starting HDFS Cluster...")
     val baseDir = new File(PathUtils.getTestDir(getClass()), "miniHDFS")
+    import org.apache.hadoop.fs.FileUtil
+    FileUtil.fullyDelete(baseDir)
     print(baseDir)
     val conf = new Configuration()
     conf.set(MiniDFSCluster.HDFS_MINIDFS_BASEDIR, baseDir.getAbsolutePath())
