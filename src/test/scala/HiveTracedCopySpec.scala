d81ec863268b782c868430b0519de6e54cbf997f
nidhal.saadaoui1@esprit.tn <Azerty.199346>
2019-11-27 10:34:48 +0100

    before all and after all implementation

--- src/test/scala/HiveTracedCopySpec.scala
+++ src/test/scala/HiveTracedCopySpec.scala
@@ -2,86 +2,106 @@ import configuration.{ArgFileConf, Config}
 import hadoopIO.{HDFSCluster, HDFSHelper}
 import org.apache.hadoop.fs.Path
 import org.apache.spark.sql.SparkSession
-import org.scalatest.{FlatSpec, Matchers}
-import sevices.{CopyFileService, CopyHiveTableService}
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FlatSpec, Suite}
+import sevices.CopyHiveTableService
 
-class HiveTracedCopySpec extends FlatSpec with Matchers with HDFSCluster {
+class HiveTracedCopySpec extends FlatSpec with Suite with HDFSCluster with BeforeAndAfterEach with BeforeAndAfterAll {
 
   behavior of "TracedCopy"
 
-  //BeforeAll
-
-  startHDFS
-
-  val clusterURI = getNameNodeURI
-  val rootDirectory = getNameNodeURI + "/test"
-  val hdfsHelper = HDFSHelper(clusterURI)
-
-  hdfsHelper.hdfs.mkdirs(new Path(clusterURI + "/test/source"))
-  hdfsHelper.hdfs.mkdirs(new Path(clusterURI + "/test/destination"))
-  hdfsHelper.hdfs.mkdirs(new Path(clusterURI + "/test/trace"))
-
-  hdfsHelper.hdfs.create(new Path(rootDirectory + "/source/HDFSTestFile2.txt"))
-  hdfsHelper.hdfs.create(new Path(rootDirectory + "/source/HDFSTestFile3.txt"))
-
-  hdfsHelper.hdfs.create(new Path(rootDirectory + "/trace/trace.csv"))
-  hdfsHelper.hdfs.create(new Path(rootDirectory + "/config.xml"))
-
-  implicit val spark: SparkSession = SparkSession
-    .builder()
-    .appName("SparkApp")
-    .config("spark.master", "local[*]")
-    .enableHiveSupport()
-    .getOrCreate()
+  var clusterURI: String = _
+  var rootDirectory: String = _
+  var hdfsHelper: HDFSHelper = _
+  implicit var spark: SparkSession = _
+  var config: Config = _
 
 
-  spark.sql("TRUNCATE TABLE TRACETABLE")
+  override def beforeAll(): Unit = {
+    startHDFS()
+    clusterURI = getNameNodeURI
+    rootDirectory = getNameNodeURI + "/test"
+    hdfsHelper = HDFSHelper(clusterURI)
+
+    hdfsHelper.hdfs.mkdirs(new Path(clusterURI + "/test/source"))
+    hdfsHelper.hdfs.mkdirs(new Path(clusterURI + "/test/destination"))
+    hdfsHelper.hdfs.mkdirs(new Path(clusterURI + "/test/trace"))
 
 
-  hdfsHelper.writeInto("HDFSTestFile2 content", rootDirectory + "/source/HDFSTestFile2.txt", hdfsHelper.hdfs)
-  hdfsHelper.writeInto("HDFSTestFile3 content", rootDirectory + "/source/HDFSTestFile3.txt", hdfsHelper.hdfs)
+    hdfsHelper.hdfs.create(new Path(rootDirectory + "/trace/trace.csv"))
+    hdfsHelper.hdfs.create(new Path(rootDirectory + "/config.xml"))
 
-  ////////////////////////////////////////////////////////////////////////////////
-  val configPath = rootDirectory + "/config.xml"
-  val configSource = scala.io.Source.fromFile("src/test/resources/fichierArguments.xml")
-  val lines = configSource.mkString
-  hdfsHelper.writeInto(lines, configPath, hdfsHelper.hdfs)
+    spark = SparkSession
+      .builder()
+      .appName("SparkApp")
+      .config("spark.master", "local[*]")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    val configPath = rootDirectory + "/config.xml"
+    val configSource = scala.io.Source.fromFile("src/test/resources/fichierArguments.xml")
+    val lines = configSource.mkString
+    hdfsHelper.writeInto(lines, configPath, hdfsHelper.hdfs)
 
 
-  val stream = hdfsHelper.hdfs.open(new Path(configPath))
+    val stream = hdfsHelper.hdfs.open(new Path(configPath))
 
-  val configFileContent: String = scala.io.Source.fromInputStream(stream).takeWhile(_ != null).mkString
-  val config: Config = ArgFileConf.loadConfig(configFileContent)
+    val configFileContent: String = scala.io.Source.fromInputStream(stream).takeWhile(_ != null).mkString
+    config = ArgFileConf.loadConfig(configFileContent)
+
+    super.beforeEach()
+  }
+
+  println("out")
+
+  override def afterAll(): Unit = {
+    shutdownHDFS()
+    spark.close()
+  }
+
+
+  override def beforeEach() {
+    spark.sql("TRUNCATE TABLE TRACETABLE")
+    super.beforeEach()
+  }
+
 
   it should "make traced copy with  an empty destination and trace " in {
 
+    hdfsHelper.hdfs.create(new Path(rootDirectory + "/source/HDFSTestFile2.txt"))
+    hdfsHelper.hdfs.create(new Path(rootDirectory + "/source/HDFSTestFile3.txt"))
+
+    hdfsHelper.writeInto("HDFSTestFile2 content", rootDirectory + "/source/HDFSTestFile2.txt", hdfsHelper.hdfs)
+    hdfsHelper.writeInto("HDFSTestFile3 content", rootDirectory + "/source/HDFSTestFile3.txt", hdfsHelper.hdfs)
 
     CopyHiveTableService(clusterURI).tabletracedCopy(config.sourceDirectory, config.destinationDirectory)
-
+    spark.sql("SELECT * FROM TRACETABLE").show()
 
 
   }
   it should "make traced copy when file checksum already exist in destination " in {
 
-    hdfsHelper.hdfs.create(new Path(rootDirectory + "/source/HDFSTestFile33.txt"))
-    hdfsHelper.writeInto("HDFSTestFile3 content", rootDirectory + "/source/HDFSTestFile33.txt", hdfsHelper.hdfs)
+    hdfsHelper.hdfs.create(new Path(rootDirectory + "/source/HDFSTestFile4.txt"))
+    hdfsHelper.hdfs.create(new Path(rootDirectory + "/destination/HDFSTestFile44.txt"))
+
+    hdfsHelper.writeInto("HDFSTestFile4 content", rootDirectory + "/source/HDFSTestFile4.txt", hdfsHelper.hdfs)
+    hdfsHelper.writeInto("HDFSTestFile4 content", rootDirectory + "/destination/HDFSTestFile44.txt", hdfsHelper.hdfs)
 
     CopyHiveTableService(clusterURI).tabletracedCopy(config.sourceDirectory, config.destinationDirectory)
-
+    spark.sql("SELECT * FROM TRACETABLE").show()
 
   }
 
   it should "make traced copy when file name already exist in destination " in {
 
-    hdfsHelper.hdfs.create(new Path(rootDirectory + "/source/HDFSTestFile3.txt"))
-    hdfsHelper.writeInto("HDFSTestFile3 new content", rootDirectory + "/source/HDFSTestFile3.txt", hdfsHelper.hdfs)
+    hdfsHelper.hdfs.create(new Path(rootDirectory + "/source/HDFSTestFile5.txt"))
+    hdfsHelper.hdfs.create(new Path(rootDirectory + "/destination/HDFSTestFile5.txt"))
+
+    hdfsHelper.writeInto("HDFSTestFile5 content", rootDirectory + "/source/HDFSTestFile5.txt", hdfsHelper.hdfs)
+    hdfsHelper.writeInto("HDFSTestFile55 content", rootDirectory + "/destination/HDFSTestFile5.txt", hdfsHelper.hdfs)
 
     CopyHiveTableService(clusterURI).tabletracedCopy(config.sourceDirectory, config.destinationDirectory)
     spark.sql("SELECT * FROM TRACETABLE").show()
-    shutdownHDFS
+
   }
 
-  //AfterAll
-
-  //
 }
