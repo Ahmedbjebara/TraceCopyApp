dcbdca2db65fe63ec73044a2911af2b020438ad5
nidhal.saadaoui1@esprit.tn <Azerty.199346>
2019-11-28 11:02:19 +0100

    data process spec

--- src/main/scala/ProcessDataFiles.scala
+++ src/main/scala/ProcessDataFiles.scala
@@ -1,70 +1,74 @@
-import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types._
+import hadoopIO.HDFSHelper
+import org.apache.hadoop.fs.Path
+import org.apache.spark.sql.functions.{input_file_name, _}
+import org.apache.spark.sql.types.{StructType, _}
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import scala.io.Source
-import org.apache.spark.sql.functions.input_file_name
 
 
-object ProcessDataFiles {
+case class ProcessDataFiles(uri: String) {
+
+  val hdfsHelper = HDFSHelper(uri)
+
+  def parseSchema(schemaFile: String): StructType = {
+    csvFileToStructType(schemaFile)
+  }
+
+  def load(destinationDirectory: String, schema: StructType, readMode: String, sourceFileFormat: String)(implicit spark: SparkSession): DataFrame = {
+
+    spark.read.format("csv")
+      .option("header", "true")
+      .option("mode", readMode)
+      .schema(schema)
+      .load(destinationDirectory)
+  }
+
+  def process(loadedDataframe: DataFrame)(implicit spark: SparkSession): DataFrame = {
+    tagWithFileName(loadedDataframe)
+  }
+
+  def tagWithFileName(df: DataFrame)(implicit spark: SparkSession): DataFrame = {
+    spark.udf.register("file_name", (path: String) => path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf(".")))
+    df.withColumn("fileName", callUDF("file_name", input_file_name()))
+  }
 
 
-    def parseSchema(schemaFile: String): StructType = {
-      csvFileToStructType(schemaFile)
-    }
+  def write(processedDataframe: DataFrame, partitionColumn: String, resultFile: String) {
 
-    def load(destinationDirectory: String ,schema : StructType ,readMode: String, sourceFileFormat: String)(implicit spark: SparkSession): DataFrame = {
+    processedDataframe.show()
+    processedDataframe
+      .write
+      .partitionBy(partitionColumn)
+      .parquet(resultFile)
+  }
 
-      spark.read.format("csv")
-        .option("header", "true")
-        .option("mode", readMode)
-        .schema(schema)
-        .load(destinationDirectory)
-    }
+  private def csvFileToStructType(schemaFile: String): StructType = {
 
-    def process(loadedDataframe: DataFrame)(implicit spark: SparkSession): DataFrame = {
-      //loadedDataframe.transform(tagWithFileName())
-      tagWithFileName(loadedDataframe)
-    }
+    val stream = hdfsHelper.hdfs.open(new Path(schemaFile))
 
-    def tagWithFileName(df: DataFrame)(implicit spark: SparkSession): DataFrame = {
-      spark.udf.register("file_name", (path: String) => path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf(".")))
-      df.withColumn("fileName", callUDF("file_name", input_file_name()))
-    }
+    val readLines = scala.io.Source.fromInputStream(stream)
 
+    val content: String = readLines.takeWhile(_ != null).mkString
+    readLines.close()
+    val schematoArrayofString = content.split(",").map(line => {
+      val name = line.substring(line.indexOf("\"") + 1, line.indexOf(("\""), line.indexOf("\"") + 1))
+      val typeField = line.substring(line.indexOf(("\""), line.indexOf(":")) + 1, line.lastIndexOf("\""))
 
-    def write(processedDataframe: DataFrame, partitionColumn: String, resultFile: String) {
+      StructField(name, replaceTypeWithDatatype(typeField), false)
 
-      processedDataframe
-        .write
-        .partitionBy(partitionColumn)
-        .parquet(resultFile)
-    }
-
-    private def csvFileToStructType(schemaFile: String): StructType = {
-
-      val sourceFile = Source.fromFile(schemaFile).mkString
-      //TODO use specific library for parsing schema
-      val schematoArrayofString = sourceFile.split(",").map(line => {
-        val name = line.substring(line.indexOf("\"") + 1, line.indexOf(("\""), line.indexOf("\"") + 1))
-        val typeField = line.substring(line.indexOf(("\""), line.indexOf(":")) + 1, line.lastIndexOf("\""))
-
-        StructField(name, replaceTypeWithDatatype(typeField), false)
-
-      })
-      StructType.apply(schematoArrayofString)
-
-    }
-
-
-    private def replaceTypeWithDatatype(typeName: String): DataType = {
-      typeName match {
-        case "String" => StringType
-        case "Integer" => IntegerType
-        case "Double" => DoubleType
-        case "Boolean" => BooleanType
-
-      }
-    }
+    })
+    StructType.apply(schematoArrayofString)
 
   }
+
+
+  private def replaceTypeWithDatatype(typeName: String): DataType = {
+    typeName match {
+      case "String" => StringType
+      case "Integer" => IntegerType
+      case "Double" => DoubleType
+      case "Boolean" => BooleanType
+
+    }
+  }
+
+}
---
+++ src/test/scala/ProcessDataSpec.scala
@@ -0,0 +1,77 @@
+import configuration.{ArgFileConf, Config}
+import hadoopIO.{HDFSCluster, HDFSHelper}
+import org.apache.hadoop.fs.Path
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FlatSpec, Suite}
+
+class ProcessDataSpec extends FlatSpec with Suite with HDFSCluster with BeforeAndAfterEach with BeforeAndAfterAll {
+
+  behavior of "ProcessData"
+
+  var clusterURI: String = _
+  var rootDirectory: String = _
+  var hdfsHelper: HDFSHelper = _
+  implicit var spark: SparkSession = _
+  var config: Config = _
+
+  override def beforeAll(): Unit = {
+    startHDFS()
+    clusterURI = getNameNodeURI
+    rootDirectory = getNameNodeURI + "/test"
+    hdfsHelper = HDFSHelper(clusterURI)
+
+    hdfsHelper.hdfs.mkdirs(new Path(clusterURI + "/test/destination"))
+    hdfsHelper.hdfs.mkdirs(new Path(clusterURI + "/test/schema"))
+
+
+    hdfsHelper.hdfs.create(new Path(rootDirectory + "/config.xml"))
+    hdfsHelper.hdfs.create(new Path(rootDirectory + "/schema/schemaout.csv"))
+
+    hdfsHelper.hdfs.create(new Path(clusterURI + "/test/destination/testFile1.csv"))
+
+    spark = SparkSession
+      .builder()
+      .appName("SparkApp")
+      .config("spark.master", "local[*]")
+      .enableHiveSupport()
+      .getOrCreate()
+
+    val configPath = rootDirectory + "/config.xml"
+    val configSource = scala.io.Source.fromFile("src/test/resources/fichierArguments.xml")
+    val schemaSource = scala.io.Source.fromFile("src/test/resources/schemaout.txt")
+    val testFile1Source = scala.io.Source.fromFile("src/test/resources/exportVersion1.csv")
+    val configContent = configSource.mkString
+    val schemaContent = schemaSource.mkString
+    val testFile1Content = testFile1Source.mkString
+    hdfsHelper.writeInto(configContent, configPath, hdfsHelper.hdfs)
+    hdfsHelper.writeInto(schemaContent, rootDirectory + "/schema/schemaout.csv", hdfsHelper.hdfs)
+    hdfsHelper.writeInto(testFile1Content, clusterURI + "/test/destination/testFile1.csv", hdfsHelper.hdfs)
+
+
+    val stream = hdfsHelper.hdfs.open(new Path(configPath))
+
+    val configFileContent: String = scala.io.Source.fromInputStream(stream).takeWhile(_ != null).mkString
+    config = ArgFileConf.loadConfig(configFileContent)
+
+    super.beforeAll()
+  }
+
+  override def afterAll(): Unit = {
+   super.afterAll()
+  }
+
+  override def beforeEach() {
+    super.beforeEach()
+  }
+
+  it should "make traced copy with  an empty destination and trace " in {
+
+    val schema = ProcessDataFiles(clusterURI).parseSchema(config.schemaFile)
+    val loadedData: DataFrame = ProcessDataFiles(clusterURI).load(config.destinationDirectory, schema, config.readMode, config.sourceFileFormat)
+    val processedData: DataFrame = ProcessDataFiles(clusterURI).process(loadedData)
+    ProcessDataFiles(clusterURI).write(processedData, config.partitionColumn, config.resultFile)
+
+
+  }
+
+}
